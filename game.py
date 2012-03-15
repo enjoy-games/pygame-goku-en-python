fd1ff9436a70865a1df68350fa084ca3ca32df54
sergio <devnull@localhost>
2012-03-15 12:52:17 +0100

    2 Movimiento manual

--- game.py
+++ game.py
@@ -1,49 +1,74 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
+from pygame.locals import *
+from pygame.sprite import Sprite
 
-# Variables.
-personajeX = 45
-personajeY = 45
-salir = False
+class Personaje(Sprite):
+	def __init__(self):
+		self.image = personaje = pygame.image.load("Imagenes/goku.png").convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.move_ip(200, 300)
+	def update(self):
+		teclas = pygame.key.get_pressed()
+		if teclas[K_LEFT]:
+			self.rect.x -= 10
+		elif teclas[K_RIGHT]:
+			self.rect.x += 10
 
-# Objetos
-temporizador = pygame.time.Clock()
+		if teclas[K_UP]:
+			self.rect.y -= 10
+		elif teclas[K_DOWN]:
+			self.rect.y += 10
 
-# Establezco la pantalla.
-screen = pygame.display.set_mode((800,600))
+class Kamehameha(Sprite):
+	def __init__(self):
+		self.image = kamehameha = pygame.image.load("Imagenes/kamehameha.gif").convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.move_ip(-200, -300)
+	def update(self):
+		teclas = pygame.key.get_pressed()
+		if teclas[K_SPACE]:
+			self.rect.x = personaje.rect.x
+			self.rect.y = personaje.rect.y
+		if self.rect.x > -200:
+			self.rect.x -= 20
+		if self.rect.y > -300:
+			self.rect.y -= 20
+			
 
-# Establezco el título.
-pygame.display.set_caption("Movimiento automático")
+if __name__ == '__main__':
+	# Variables.
+	salir = False
 
-# Creo dos objetos surface.
-fondo = pygame.image.load("Imagenes/fondo.jpg").convert()
-personaje = pygame.image.load("Imagenes/goku.png").convert_alpha()
-# .convert() convierten la superficie a un formato de color que permite imprimirlas mucho mas rápido.
+	# Establezco la pantalla.
+	screen = pygame.display.set_mode((800,600))
 
-# Imprimo las imágenes sobre la pantalla.
-screen.blit(fondo, (0,0))
-screen.blit(personaje, (personajeX,personajeY))
-pygame.display.flip()
+	# Establezco el título.
+	pygame.display.set_caption("Movimiento manual")
 
+	# Creo do objeto surface.
+	fondo = pygame.image.load("Imagenes/fondo.jpg").convert()
+	# .convert() convierten la superficie a un formato de color que permite imprimirlas mucho mas rápido.
 
-# Movimiento del personaje.
-while not salir:
-	personajeX += 10
-        personajeY += 10
+	# Objetos
+	temporizador = pygame.time.Clock()
+	personaje = Personaje()
+	kamehameha = Kamehameha()
 
-	if personajeX > 800:
-		personajeX = -80
+	# Movimiento del personaje.
+	while not salir:
+		personaje.update()
+		kamehameha.update()
 
-        if personajeY > 600:
-		personajeY = -80
+		# actualizacion grafica
+		screen.blit(fondo, (0, 0))
+		screen.blit(personaje.image, personaje.rect)
+		screen.blit(kamehameha.image, kamehameha.rect)
+		pygame.display.flip()
 
-	screen.blit(personaje, (personajeX,personajeY))
-	pygame.display.flip()
-	temporizador.tick(60)
-
-	screen.blit(fondo, (0,0))
-
-	for evento in pygame.event.get():
-		if evento.type == pygame.QUIT:
-			salir = True
+		temporizador.tick(60)
+		# gestion de eventos
+		for evento in pygame.event.get():
+			if evento.type == pygame.QUIT:
+				salir = True
